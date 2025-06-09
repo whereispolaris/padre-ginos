8586ffc3c65ce40616c985b2209c04b72b4f01f2
santiagoenciso <whereispolaris@gmail.com>
2025-06-09 10:23:50 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni",
+      description: "Mozza and PEps",
+    }),
+    React.createElement(Pizza, {
+      name: "Ameircano Pizza",
+      description: "Hot dogs and french fries",
+    }),
+    React.createElement(Pizza, {
+      name: "Hawaiian",
+      description: "Ham and pineapple",
+    }),
+    React.createElement(Pizza, {
+      name: "Chicken Pizza",
+      description: "Chicky nuggies",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
