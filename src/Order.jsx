59cbd26cccf6fc7b45178223602d012e3ec9f774
santiagoenciso <whereispolaris@gmail.com>
2025-06-14 11:19:50 -0500

    Added useState hook to allow form elements to update correctly on re-render.

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,8 +1,11 @@
 import Pizza from "./Pizza";
+import { useState } from "react";
+
+
 
 export default function Order() {
-  const pizzaType = "pepperoni";
-  const pizzaSize = "M";
+    const [pizzaType, setPizzaType] = useState("pepperoni");
+    const [pizzaSize, setPizzaSize] = useState("medium");
   return (
     <div className="order">
       <h2>Create Order</h2>
@@ -10,7 +13,11 @@ export default function Order() {
         <div>
           <div>
             <label htmlFor="pizza-type">Pizza Type</label>
-            <select name="pizza-type" value={pizzaType}>
+            <select
+                onChange={(e) => setPizzaType(e.target.value)}
+                name="pizza-type"
+                value={pizzaType}
+                >
               <option value="pepperoni">The Pepperoni Pizza</option>
               <option value="hawaiian">The Hawaiian Pizza</option>
               <option value="big_meat">The Big Meat Pizza</option>
@@ -26,6 +33,7 @@ export default function Order() {
                   name="pizza-size"
                   value="S"
                   id="pizza-s"
+                  onChange={(e) => setPizzaSize(e.target.value)}
                 />
                 <label htmlFor="pizza-s">Small</label>
               </span>
@@ -36,6 +44,7 @@ export default function Order() {
                   name="pizza-size"
                   value="M"
                   id="pizza-m"
+                  onChange={(e) => setPizzaSize(e.target.value)}
                 />
                 <label htmlFor="pizza-m">Medium</label>
               </span>
@@ -46,6 +55,7 @@ export default function Order() {
                   name="pizza-size"
                   value="L"
                   id="pizza-l"
+                  onChange={(e) => setPizzaSize(e.target.value)}
                 />
                 <label htmlFor="pizza-l">Large</label>
               </span>
